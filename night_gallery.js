6739804dfd74f472fa5df466efec9727ff0da630
Aurelia Roumesy <aurelia.roumesy@gmail.com>
2019-03-06 15:31:45 +0100

    day and night gallery version JS

--- night_gallery.js
+++ night_gallery.js
@@ -1,11 +1,37 @@
-jQuery(function(){
-    $(function () {
-    $(window).scroll(function () { //Fonction appelée quand on descend la page
-    if ($(this).scrollTop() > 200 ) {  // Quand on est à 200pixels du haut de page,
-    $('#scrollUp').css('right','10px'); // Replace à 10pixels de la droite l'image
-    } else { 
-    $('#scrollUp').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+  }
+  
+  function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(modalId, n) {
+    showSlides(modalId, slideIndex += n);
+  }
+  
+  function currentSlide(modalId, n) {
+    showSlides(modalId, slideIndex = n);
+  }
+  
+  function showSlides(modalId, n) {
+    var i;
+    var modal = document.getElementById(modalId);
+    var slides = modal.getElementsByClassName("mySlides");
+    var dots = modal.getElementsByClassName("demo");
+    var captionText = modal.getElementsByClassName("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    });
-    });
-    });
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
