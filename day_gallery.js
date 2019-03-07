10129dc94bb31bba246a9390c31c78aeba68bc3d
Romain-Dailly <loops51@hotmail.com>
2019-03-07 13:58:13 +0100

    testing again

--- day_gallery.js
+++ day_gallery.js
@@ -14,7 +14,7 @@ jQuery(function(){
 $(document).on("scroll", function () {
     var pageTop = $(document).scrollTop()
     var pageBottom = pageTop + $(window).height()
-    var tags = $("section")
+    var tags = $(".row")
   
     for (var i = 0; i < tags.length; i++) {
       var tag = tags[i]
